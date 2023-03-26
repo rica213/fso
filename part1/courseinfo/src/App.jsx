8b2e9d92833d3205ef2db61e4f3585f474771f3b
rica213 <25f5k252l@mozmail.com>
2023-03-27 00:57:18 +0300

    1.3. Finish course info step 3 Use of object for the props

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,21 +4,27 @@ import Total from "./components/Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part={[part1, part2, part3]}
-        content={[exercises1, exercises2, exercises3]}
+        part={[part1.name, part2.name, part3.name]}
+        content={[part1.exercises, part2.exercises, part3.exercises]}
       />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
